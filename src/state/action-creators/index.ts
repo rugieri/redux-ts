704437e82a655ae318348d5d89b79a9b2df7091a
rugieri <rugieri.ronaldo@gmail.com>
2021-06-05 16:11:13 -0300

    applied typings to dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { Dispatch } from 'redux';  
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
 export const search_repos = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({ 
       type: ActionType.SEARCH_REPOS
     });
