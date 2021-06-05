81db3e377c9aaa80f5e42ce28d8239a6afdcf474
rugieri <rugieri.ronaldo@gmail.com>
2021-06-05 16:04:55 -0300

    added request logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,13 +2,28 @@ import axios from 'axios';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
-const search_repos = (term: string) => {
+export const search_repos = (term: string) => {
   return async (dispatch: any) => {
     dispatch({ 
       type: ActionType.SEARCH_REPOS
     });
 
     try {
+      const { data } = await axios.get ('https://registry.npmjs.org/-/v1/search',
+       { 
+      params: {
+        text: term
+      },
+     });
+
+     const names = data.objects.map((result: any) =>{
+       return result.package.name;
+     });
+
+     dispatch({
+       type: ActionType.SEARCH_REPOS_SUCCESS,
+       payload: names
+     })
 
     } catch (err) {
       dispatch({
